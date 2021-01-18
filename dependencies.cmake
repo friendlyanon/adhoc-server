c8a88069bfa9910f9928c9aed897837898a0f611
friendlyanon <friendlyanon@users.noreply.github.com>
2021-01-18 19:15:44 +0000

    Add daw_link_json dependency

--- dependencies.cmake
+++ dependencies.cmake
@@ -2,3 +2,8 @@ ubpm_dependency(
     blobify
     GITHUB friendlyanon/blobify master 571efd1
 )
+
+ubpm_dependency(
+    daw_json_link
+    GITHUB friendlyanon/daw_json_link cmake-release 5ff9ea0
+)
