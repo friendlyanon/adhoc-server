fc614edd62f0c0c538db11b3ffc17703bfd086d1
friendlyanon <friendlyanon@users.noreply.github.com>
2021-08-29 16:51:24 +0200

    Implement server creation for Windows

--- source/server.h
+++ source/server.h
@@ -1,3 +1,14 @@
 #pragma once
 
-/* TODO: come up with a server API */
+#include <stdbool.h>
+#include <stddef.h>
+
+/**
+ * @brief Returns the size of the struct to be allocated for ::create_server.
+ */
+size_t server_size(void);
+
+/**
+ * @brief Creates the server and writes it out to the \c result_server buffer.
+ */
+bool create_server(void* result_server);
--- source/server.nt.c
+++ source/server.nt.c
@@ -1,5 +1,75 @@
 #include "server.nt.h"
 
 #include <MSWSock.h>
+#include <stdio.h>
+#include <string.h>
 
-/* TODO: implement server */
+typedef struct server {
+  bool ok;
+  bool server_started;
+  HANDLE completion_port;
+} server;
+
+size_t server_size()
+{
+  return sizeof(server);
+}
+
+static server startup(server server)
+{
+  if (!server.ok) {
+    return server;
+  }
+
+  WSADATA wsa_data;
+  int startup_result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+  if (startup_result != 0) {
+    fprintf(stderr, "WSAStartup: %d\n", startup_result);
+    server.ok = false;
+    return server;
+  }
+  server.server_started = true;
+
+  if (LOBYTE(wsa_data.wVersion) != 2 || HIBYTE(wsa_data.wVersion) != 2) {
+    fprintf(stderr,
+            "Could not find a usable version of Winsock.dll\n"
+            "  Expected: 2.2\n"
+            "  Actual: %d.%d\n",
+            (int)LOBYTE(wsa_data.wVersion),
+            (int)HIBYTE(wsa_data.wVersion));
+    server.ok = false;
+  }
+
+  return server;
+}
+
+static server create_completion_port(server server)
+{
+  if (!server.ok) {
+    return server;
+  }
+
+  HANDLE completion_port =
+      CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+  if (completion_port == NULL) {
+    fprintf(stderr, "CreateIoCompletionPort: %lu\n", GetLastError());
+    server.ok = false;
+  } else {
+    server.completion_port = completion_port;
+  }
+
+  return server;
+}
+
+bool create_server(void* result_server)
+{
+  server server = (struct server) {
+      .ok = true,
+      .completion_port = INVALID_HANDLE_VALUE,
+  };
+  server = startup(server);
+  server = create_completion_port(server);
+
+  memcpy(result_server, &server, server_size());
+  return server.ok;
+}
