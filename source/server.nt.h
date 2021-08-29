1dafe5718c8d2e6ba983b7030438d61519f7c57a
friendlyanon <friendlyanon@users.noreply.github.com>
2021-08-29 15:13:17 +0200

    Use @file command in explainer of server.nt.h

--- source/server.nt.h
+++ source/server.nt.h
@@ -5,6 +5,8 @@
 #include "server.h"
 
 /**
+ * @file
+ *
  * This header is a proxy header, whose only purpose is to include
  * <tt>&lt;WinSock2&gt;</tt> before anything else is included in
  * <tt>server.nt.c</tt>. This is necessary, because <tt>&lt;MSWSock.h&gt;</tt>
