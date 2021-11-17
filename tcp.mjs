31e47f15352e7da5028ec153adda088061975898
friendlyanon <friendlyanon@users.noreply.github.com>
2021-11-17 13:51:41 +0100

    Handle duplicate TCP connections

--- tcp.mjs
+++ tcp.mjs
@@ -5,9 +5,16 @@ import { users } from "./users.mjs";
  * @param {import("net").Socket} connection
  */
 function onConnection(connection) {
-  console.log("New connection from %s", connection.remoteAddress);
+  const { remoteAddress } = connection;
+  if (users.has(remoteAddress)) {
+    console.log("Duplicate connection from %s", remoteAddress);
+    connection.end();
+    return;
+  }
+
+  console.log("New connection from %s", remoteAddress);
   const userState = {};
-  users.set(connection.remoteAddress, userState);
+  users.set(remoteAddress, userState);
   connection.on("data", (chunk) => {
     // TODO implement the adhoc server
   });
