d079141daf392b0431673eae4d4eca4dd39ca578
friendlyanon <friendlyanon@users.noreply.github.com>
2021-12-04 19:55:06 +0100

    Actually return an u32 from `ipToUint32`

--- util.mjs
+++ util.mjs
@@ -14,5 +14,5 @@ export const asyncWrite = (stream, data) => new Promise((resolve, reject) => {
  */
 export const ipToUint32 = (ip) => {
   const [_1, _2, _3, _4] = ip.split(".").map(Number);
-  return _1 << 24 | _2 << 16 | _3 << 8 | _4;
+  return (_1 << 24 | _2 << 16 | _3 << 8 | _4) >>> 0;
 };
